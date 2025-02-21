16245902b47534b65845501416248e342b9f197f
nino_rajacic <nino@foxyvoxel.io>
2025-02-21 07:51:58 +0100

    TMP Sprite Asset creation and cleanup

---
+++ Assets/FoxyVoxel/NSMedieval/Modding/ModInfoData.cs
@@ -0,0 +1,10 @@
+﻿namespace NSMedieval.Modding
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "ModInfoData", menuName = "Going Medeival/Mod Info Data", order = 1)]
+    public class ModInfoData : ScriptableObject
+    {
+        public string modName = "MyMod";
+    }
+}
